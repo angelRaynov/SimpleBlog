9145517d6d25c4362045348b663794c19ee0f4c6
Angel Raynov <40572511+angelRaynov@users.noreply.github.com>
2018-12-02 00:06:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on November 30, 2018, 11:38 am.
---
+++ src/AppBundle/Controller/UsersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+
+
+
+class UsersController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted()){
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute("security_login");
+        }
+        return $this->render("user/register.html.twig");
+    }
+}
